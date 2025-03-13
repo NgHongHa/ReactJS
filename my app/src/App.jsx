6023d0178465d17b21c827b39fd33da43c314df3
NgHongHa <ha2k3.dev@gmail.com>
2025-03-13 10:04:38 +0700

    _ props.children_

--- my app/src/App.jsx
+++ my app/src/App.jsx
@@ -1,6 +1,7 @@
 import { Mydata } from "../data";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
+import TabButton from "./components/TabButton";
 // MainContent.propTypes = {
 //   image: propTypes.string.isRequired,
 // };
@@ -21,6 +22,13 @@ function App() {
             <MainContent {...Mydata[2]} />
             <MainContent {...Mydata[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>examples</h2>
+          <menu>
+            <TabButton>nút cách 1</TabButton>
+            {/* <TabButton batky="nút cách 2"></TabButton> */}
+          </menu>
         </section>
       </main>
     </>
